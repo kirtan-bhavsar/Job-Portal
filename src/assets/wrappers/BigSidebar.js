5f7df84ccca3a343344cc7b92fc5e8d2526f086c
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-05-29 00:36:11 +0530

    designed_BigSidebar.jsx_addedCSSForBigSidebar_Dt:29052025

--- src/assets/wrappers/BigSidebar.js
+++ src/assets/wrappers/BigSidebar.js
@@ -4,59 +4,63 @@ const Wrapper = styled.aside`
   display: none;
   @media (min-width: 992px) {
     display: block;
+  }
+  .sidebar-container {
+    background: var(--background-secondary-color);
+    /* background: red; */
     box-shadow: 1px 0px 0px 0px rgba(0, 0, 0, 0.1);
-    .sidebar-container {
-      background-color: var(--background-secondary-color);
-      min-height: 100vh;
-      height: 100%;
-      width: 250px;
-      margin-left: -250px;
-      transition: margin-left 0.3s ease-in-out;
-    }
-    .content {
-      position: sticky;
-      top: 0;
-    }
-    .show-sidebar {
-      margin-left: 0;
-    }
-    header {
-      height: 6rem;
-      display: flex;
-      align-items: center;
-      padding-left: 2.5rem;
-    }
-    .nav-links {
-      padding-top: 2rem;
-      display: flex;
-      flex-direction: column;
-    }
-    .nav-link {
-      display: flex;
-      align-items: center;
-      color: var(--text-secondary-color);
-      padding: 1rem 0;
-      padding-left: 2.5rem;
-      text-transform: capitalize;
-      transition: padding-left 0.3s ease-in-out;
-    }
-    .nav-link:hover {
-      padding-left: 3rem;
-      color: var(--primary-500);
-      transition: var(--transition);
-    }
-    .icon {
-      font-size: 1.5rem;
-      margin-right: 1rem;
-      display: grid;
-      place-items: center;
-    }
-    .active {
-      color: var(--primary-500);
-    }
-    .pending {
-      background: var(--background-color);
-    }
+    min-height: 100vh;
+    height: 100%;
+    width: 250px;
+    /* position: sticky; */
+    margin-left: -250px;
+    transition: margin-left 0.3s ease-in-out;
+  }
+  .content {
+    position: sticky;
+    top: 0;
+  }
+  .show-sidebar {
+    /* background-color: red; */
+    margin-left: 0;
+  }
+  header {
+    /* background-color: red; */
+    height: 6rem;
+    display: flex;
+    align-items: center;
+    padding-left: 2.5rem;
+  }
+  .nav-links {
+    display: flex;
+    flex-direction: column;
+    padding-top: 2rem;
+    /* background-color: red; */
+    /* padding: 1rem 0; */
+    /* padding-left: 2.5rem; */
+  }
+  .nav-link {
+    display: flex;
+    align-items: center;
+    padding: 1rem 0;
+    padding-left: 2.5rem;
+    color: var(--text-secondary-color);
+    transition: padding-left 0.3s ease-in-out;
+    text-transform: capitalize;
+  }
+  .nav-link:hover {
+    padding-left: 3rem;
+    color: var(--primary-500);
+    transition: var(--transition);
+  }
+  .icon {
+    font-size: 1.5rem;
+    margin-right: 1rem;
+    display: grid;
+    align-items: center;
+  }
+  .active {
+    color: var(--primary-500);
   }
 `;
 export default Wrapper;
