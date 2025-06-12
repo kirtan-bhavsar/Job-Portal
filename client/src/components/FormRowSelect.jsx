8c86087679558e22e3e511b3c821d93df2d48176
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-06-12 11:28:24 +0530

    created_FormRowSelect.jsx_implementedSelectComponent_Dt:12062025

---
+++ client/src/components/FormRowSelect.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FormRowSelect = ({name,labelText,defaultValue='',list}) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className='form-label'>{labelText || name}</label>
+      <select name={name} id={name} className='form-select' defaultValue={defaultValue}>
+      {Object.values(list).map((listValue) =>{
+        return(
+          <option key={listValue} value={listValue}>{listValue}</option>
+        )
+      })}
+      </select>
+      </div>
+  )
+}
+
+export default FormRowSelect
--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -3,8 +3,8 @@ import {useNavigation, useOutlet, useOutletContext} from 'react-router-dom';
 import { Form } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/DashboardFormPage.js';
 import FormRow  from '../components/FormRow.jsx';
-import JOB_STATUS from '../../../Utils/constants.js';
-import JOB_TYPE from '../../../Utils/constants.js';
+import FormRowSelect from '../components/FormRowSelect.jsx';
+import {JOB_STATUS,JOB_TYPE} from '../../../Utils/constants.js';
 
 const AddJob = () => {
 
@@ -21,9 +21,8 @@ const AddJob = () => {
       <FormRow type='text' name='position'></FormRow>
       <FormRow type='text' name='company'></FormRow>
       <FormRow type='text' name='jobLocation' labelText='job location' defaultValue={user.location} disabled={isSubmitting}></FormRow>
-      <div className="form-row">
-
-      </div>
+      <FormRowSelect name='jobStatus' labelText='job location' defaultValue={JOB_STATUS.PENDING} list={Object.values(JOB_STATUS)}></FormRowSelect>
+      <FormRowSelect name='jobType' labelText='job type' defaultValue={JOB_TYPE.FULL_TIME} list={Object.values(JOB_TYPE)}></FormRowSelect>
       <button type='submit' className='btn btn-block form-btn'>
         {isSubmitting ? "Submitting..." : "Submit"}
       </button>
