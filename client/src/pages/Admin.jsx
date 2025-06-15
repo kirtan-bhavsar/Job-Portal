ed99f6c25a9dfc1172f86b4c4ef926248e0a80ce
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-06-15 20:19:39 +0530

    added_Admin.jsx/StatItem.jsx_implementedAminStats_Dt:15062025

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {redirect, useLoaderData} from 'react-router-dom';
 import { customFetch } from '../utils/customFetch';
 import {toast} from 'react-toastify';
+import Wrapper from '../assets/wrappers/StatsContainer.js';
+import StatItem from '../components/StatItem.jsx';
+import {FaSuitcaseRolling,FaCalendarCheck,FaUsers } from 'react-icons/fa';
 
 export const loader = async() => {
 
   try {
     const {data} = await customFetch.get('/user/admin/app-stats');
-    // console.log(response);
     return data;
   } catch (error) {
     toast.error(error?.resposne?.data?.message || "Invalid Authentication");
@@ -18,11 +20,13 @@ export const loader = async() => {
 
 const Admin = () => {
 
-  const data = useLoaderData();
-  console.log(data);
+  const {users,jobs} = useLoaderData();
 
   return (
-    <h1>Admin</h1>
+    <Wrapper>
+      <StatItem icon={<FaUsers></FaUsers>} title='users' count={users} bcg='#e9b949' color='#fcefc7'></StatItem>
+      <StatItem icon={<FaCalendarCheck></FaCalendarCheck>} title='jobs' count={jobs} bcg='#e0e8f9' color='#647acb'></StatItem>
+    </Wrapper>
   )
 }
 
