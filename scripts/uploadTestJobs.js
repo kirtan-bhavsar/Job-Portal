7b55978509e4ccf695fda970e4465f0ceaebc739
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-06-18 22:51:30 +0530

    created_uploadTestJob.js_importingTestJobsData_Dt:18062025

--- scripts/uploadTestJobs.js
+++ scripts/uploadTestJobs.js
@@ -2,13 +2,36 @@ import dotenv from "dotenv/config";
 import User from "../Models/User.js";
 import Job from "../Models/Job.js";
 import connectDb from "../db.js";
+import { readFile } from "fs/promises";
 
 const uploadTestJob = async () => {
-  await connectDb();
+  try {
+    await connectDb();
 
-  const user = await User.findOne({ email: "cedric.diggory@gmail.com" });
+    const user = await User.findOne({ email: "cedric.diggory@gmail.com" });
 
-  console.log(user);
+    console.log(user.id);
+
+    const fetchedJobs = JSON.parse(
+      await readFile(new URL("../Utils/mockData.json", import.meta.url))
+    );
+
+    const jobs = fetchedJobs.map((job) => {
+      return { ...job, user: user.id };
+    });
+
+    await Job.deleteMany({ user: user.id });
+
+    await Job.create(jobs);
+
+    process.exit(0);
+
+    // console.log(jobs);
+  } catch (error) {
+    console.log(error);
+
+    process.exit(1);
+  }
 };
 
 uploadTestJob();
